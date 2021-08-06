43b762fbdd48e9026fba832579fac32d101ace1d
Mikhail Artamonov <maximalis171091@yandex.ru>
2021-08-06 22:52:10 +0300

    Update

--- aif-installation/modules/apps-functions.sh
+++ aif-installation/modules/apps-functions.sh
@@ -4,34 +4,35 @@
 ##                                                                  ##
 ######################################################################
 
-apps_desktop_write()
+list_of_apps_write()
 {
 	if [[ $_apps_desktop_once == "0" ]]; then
 		dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title "$_lst_of_apps_hd" --yesno "$_lst_of_apps_yn" 0 0
 		if [[ $? -eq 0 ]]; then
 			_apps_desktop_once=1
-			_user_lists=$(ls ${MOUNTPOINT}/home/ | sed "s/lost+found//")
+			echo "[Desktop Entry]" > "${MOUNTPOINT}/usr/share/applications/List_of_applications_EN.desktop"
+			echo "Encoding=UTF-8" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_EN.desktop"
+			echo "URL=https://wiki.archlinux.org/title/List_of_applications" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_EN.desktop"
+			echo "Type=Link" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_EN.desktop"
+			echo "Name=List of Apps EN" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_EN.desktop"
+			echo "Icon=text-html" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_EN.desktop"
+			echo "Categories=Network;System;" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_EN.desktop"
+			echo "Comment=List of Applications EN" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_EN.desktop"
+			echo "Keywords=applications;" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_EN.desktop"
+			echo "" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_EN.desktop"
+			chmod +x "${MOUNTPOINT}/usr/share/applications/List_of_applications_EN.desktop"
 			wait
-			for j in ${_usr_list[*]}; do
-				echo "[Desktop Entry]" > "${MOUNTPOINT}/home/${j}/List_of_applications_EN.desktop"
-				echo "Encoding=UTF-8" >> "${MOUNTPOINT}/home/${j}/List_of_applications_EN.desktop"
-				echo "URL=https://wiki.archlinux.org/title/List_of_applications" >> "${MOUNTPOINT}/home/${j}/List_of_applications_EN.desktop"
-				echo "Type=Link" >> "${MOUNTPOINT}/home/${j}/List_of_applications_EN.desktop"
-				echo "Name=List of Applications EN" >> "${MOUNTPOINT}/home/${j}/List_of_applications_EN.desktop"
-				echo "Icon=text-html" >> "${MOUNTPOINT}/home/${j}/List_of_applications_EN.desktop"
-				echo "" >> "${MOUNTPOINT}/home/${j}/List_of_applications_EN.desktop"
-				chmod +x "${MOUNTPOINT}/home/${j}/List_of_applications_EN.desktop"
-				wait
-				echo "[Desktop Entry]" > "${MOUNTPOINT}/home/${j}/List_of_applications_RU.desktop"
-				echo "Encoding=UTF-8" >> "${MOUNTPOINT}/home/${j}/List_of_applications_RU.desktop"
-				echo "URL=https://www.linuxsecrets.com/archlinux-wiki/wiki.archlinux.org/index.php/List_of_applications_(%D0%A0%D1%2583%D1%2581%D1%2581%D0%BA%D0%B8%D0%B9).html" >> "${MOUNTPOINT}/home/${j}/List_of_applications_RU.desktop"
-				echo "Type=Link" >> "${MOUNTPOINT}/home/${j}/List_of_applications_RU.desktop"
-				echo "Name=List of Applications RU" >> "${MOUNTPOINT}/home/${j}/List_of_applications_RU.desktop"
-				echo "Icon=text-html" >> "${MOUNTPOINT}/home/${j}/List_of_applications_RU.desktop"
-				echo "" >> "${MOUNTPOINT}/home/${j}/List_of_applications_RU.desktop"
-				chmod +x "${MOUNTPOINT}/home/${j}/List_of_applications_RU.desktop"
-				wait
-			done
+			echo "[Desktop Entry]" > "${MOUNTPOINT}/usr/share/applications/List_of_applications_RU.desktop"
+			echo "Encoding=UTF-8" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_RU.desktop"
+			echo "URL=https://www.linuxsecrets.com/archlinux-wiki/wiki.archlinux.org/index.php/List_of_applications_(%D0%A0%D1%2583%D1%2581%D1%2581%D0%BA%D0%B8%D0%B9).html" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_RU.desktop"
+			echo "Type=Link" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_RU.desktop"
+			echo "Name=List of Apps RU" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_RU.desktop"
+			echo "Icon=text-html" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_RU.desktop"
+			echo "Categories=Network;System;" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_RU.desktop"
+			echo "Comment=List of Applications RU" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_RU.desktop"
+			echo "Keywords=applications;" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_RU.desktop"
+			echo "" >> "${MOUNTPOINT}/usr/share/applications/List_of_applications_RU.desktop"
+			chmod +x "${MOUNTPOINT}/usr/share/applications/List_of_applications_RU.desktop"
 			wait
 			dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title "$_lst_of_apps_hd" --msgbox "$_lst_of_apps_msg" 0 0
 			wait
@@ -97,7 +98,7 @@ install_apps_menu()
 		;;
 	"8") installing_programming
 		;;
-	"9") apps_desktop_write
+	"9") list_of_apps_write
 		;;
       *) install_desktop_menu
          ;;
