fcf43a4e08604849bf69d61639b20b06990225b4
Mikhail Artamonov <maximalis171091@yandex.ru>
2021-08-15 00:50:07 +0300

    Update

--- aif-installation/config/dependences_function.sh
+++ aif-installation/config/dependences_function.sh
@@ -35,9 +35,9 @@ script_dependences_question()
 }
 git_dependences_question()
 {
-	echo -e -n "\e[1;37mДля работы скрипта требуется пакет «\e[1;git\e[1;37m».\e[1;33m Установить? (\e[1;32my\e[1;33m/\e[1;31mn\e[1;33m)? \e[0m\n"
+	echo -e -n "\e[1;37mДля работы скрипта требуется пакет «\e[1;34mgit\e[1;37m».\e[1;33m Установить? (\e[1;32my\e[1;33m/\e[1;31mn\e[1;33m)? \e[0m\n"
     echo -e -n "\e[1;37mНажатие на кнопку «\e[1;34mEnter\e[1;37m» аналогично действию «\e[1;32my\e[1;37m|\e[1;32mY\e[1;37m» - действие по умолчанию. \e[0m\n\n"
-    echo -e -n "\e[1;37mThe «\e[1;git\e[1;37m» package is required for operation.\e[1;33m To install? (\e[1;32my\e[1;33m/\e[1;31mn\e[1;33m)? \e[0m\n"
+    echo -e -n "\e[1;37mThe «\e[1;34mgit\e[1;37m» package is required for operation.\e[1;33m To install? (\e[1;32my\e[1;33m/\e[1;31mn\e[1;33m)? \e[0m\n"
     echo -e -n "\e[1;37mPressing the «\e[1;34mEnter\e[1;37m» button is similar to the «\e[1;32my\e[1;37m|\e[1;32mY\e[1;37m» action - default action.\e[0m\n"
 }
 dependences_result()
@@ -89,9 +89,9 @@ dependences_git()
 {
 	read item
     case "$item" in
-        y|Y) echo -e -n "\e[1;32mПроизводится установка пакета «dialog»...\e[0m"
+        y|Y) echo -e -n "\e[1;32mПроизводится установка пакета «git»...\e[0m"
             outin_success
-            echo -e -n "\e[1;32mThe «dialog» package is installed...\e[0m"
+            echo -e -n "\e[1;32mThe «git» package is installed...\e[0m"
             outin_success
             echo ""
             sudo pacman -Syy --noconfirm
@@ -132,29 +132,12 @@ dependences_git()
 }
 question_dialog_run()
 {
-	if [[ "${_how_shell[*]}" != "fish" ]]; then
-		sudo pacman -Qs dialog 1>/dev/null 2>/dev/null
-		if [[ $? != "0" ]]; then
-			script_dependences_question
-			dependences_result
-		fi
-		wait
-		sudo pacman -Qs git 1>/dev/null 2>/dev/null
-		if [[ $? != "0" ]]; then
-			git_dependences_question
-			dependences_git
-		fi
-		wait
-	else
-		pacman -Qs dialog 1>/dev/null 2>/dev/null
-		if [[ "$STATUS" != "0" ]]; then
-			script_dependences_question
-			dependences_result
-		fi
-		pacman -Qs git 1>/dev/null 2>/dev/null
-		if [[ "$STATUS" != "0" ]]; then
-			git_dependences_question
-			dependences_git
-		fi
-	fi
+	script_dependences_question
+	wait
+	dependences_result
+	wait
+	git_dependences_question
+	wait
+	dependences_git
+	wait
 }
