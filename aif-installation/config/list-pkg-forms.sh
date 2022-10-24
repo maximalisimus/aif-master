86711fad5878788854c3b5eb27a9a3a658346a20
maximalis171091@yandex.ru <maximalis171091@yandex.ru>
2022-10-24 09:46:10 +0300

    Update

--- aif-installation/config/list-pkg-forms.sh
+++ aif-installation/config/list-pkg-forms.sh
@@ -147,11 +147,11 @@ _theme_pkg=(gnome-icon-theme-extras breeze breeze-grub breeze-icons fontforge ${
 _ttf_theme_pkg=( ${_ttf_pkg[*]} ${_theme_pkg[*]} )
 #
 _icontheme_url="https://github.com/maximalisimus/LinuxLex-8-Icons-Theme/releases/download/v1.0/LinuxLex-8.tar.gz"
-_icontheme_pkg="./LinuxLex-8.tar.gz"
+_icontheme_pkg="/tmp/LinuxLex-8.tar.gz"
 _wallpapers_url="https://github.com/maximalisimus/Archivers-Configs-Linux/releases/download/v1.0/wallpapers.tar.gz"
-_wallpapers_pkg="./wallpapers.tar.gz"
+_wallpapers_pkg="/tmp/wallpapers.tar.gz"
 _truetype_url="https://github.com/maximalisimus/Archivers-Configs-Linux/releases/download/v1.0/MS-TrueType-Core-Fonts.tar.gz"
-_truetype_pkg="./MS-TrueType-Core-Fonts.tar.gz"
+_truetype_pkg="/tmp/MS-TrueType-Core-Fonts.tar.gz"
 #
 # gimp gimp-help-ru
 _gr_editor=(gimp)
