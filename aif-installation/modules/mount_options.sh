bac834f4d25b1fda594306a9df305a76d35ad8bb
Mikhail Artamonov <maximalis171091@yandex.ru>
2022-11-07 00:53:11 +0300

    Update

--- aif-installation/modules/mount_options.sh
+++ aif-installation/modules/mount_options.sh
@@ -7,19 +7,23 @@
 btrfs_mounted_options()
 {
 	dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title "$_btrfsMntTitle" --clear --checklist "$_btrfsMntBody" 0 0 16 \
-	"autodefrag" "$_mnt_autodefrag_bd" off \
-	"discard" "$_mnt_discard_bd" off \
-	"nodev" "$_mnt_nodev_bd" off \
-	"nosuid" "$_mnt_nosuid_bd" off \
-	"noexec" "$_mnt_noexec_bd" off \
-	"noacl" "$_mnt_noacl_bd" off \
-	"ro" "$_mnt_ro_bd" off \
+	"async" "$_mnt_async_bd" off \
 	"sync" "$_mnt_sync_bd" off \
-	"usrquota" "$_mnt_usrquota_bd" off \
-	"grpqutoa" "$_mnt_grpqutoa_bd" off \
-	"noatime" "$_mnt_noatime_bd" off \
+	"nodev" "$_mnt_nodev_bd" off \
 	"nodiratime" "$_mnt_nodiratime_bd" off \
+	"noexec" "$_mnt_noexec_bd" off \
+	"noatime" "$_mnt_noatime_bd" off \
 	"relatime" "$_mnt_relatime_bd" off \
+	"norelatime" "$_mnt_norelatime_bd" off \
+	"nosuid" "$_mnt_nosuid_bd" off \
+	"ro" "$_mnt_ro_bd" off \
+	"rw" "$_mnt_rw_bd" off \
+	"autodefrag" "$_mnt_autodefrag_bd" off \
+	"noautodefrag" "$_mnt_noautodefrag_bd" off \
+	"discard" "$_mnt_discard_bd" off \
+	"acl" "$_mnt_acl_bd" off \
+	"noacl" "$_mnt_noacl_bd" off \
+	"noatime" "$_mnt_noatime_bd" off \
 	"nodatasum" "$_mnt_nodatasum_bd" off \
 	"nospace_cache" "$_mnt_nospace_cache_bd" off \
 	"recovery" "$_mnt_recovery_bd" off \
@@ -29,8 +33,10 @@ btrfs_mounted_options()
 	"ssd_spread" "$_mnt_ssd_spread" off \
 	"compress=zlib" "$_mnt_compress_bd" off \
 	"compress=lzo" "$_mnt_compress_bd" off \
+	"compress=zstd" "$_mnt_compress_bd" off \
 	"compress=no" "$_mnt_compress_bd no" off \
 	"compress-force=zlib" "$_mnt_compress_force_bd" off \
+	"compress-force=zstd" "$_mnt_compress_force_bd" off \
 	"compress-force=lzo" "$_mnt_compress_force_bd" off 2>${BTRFS_OPTS}
 	
 	sed -i 's/ /,/g' ${BTRFS_OPTS}
@@ -44,15 +50,22 @@ btrfs_mounted_options()
 
 ext2_mounted_options()
 {
-	dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_filesystem " --checklist "$_Mnt_Body" 0 0 11 \
-	"noatime" "$_mnt_noatime_bd" off \
-	"nodiratime" "$_mnt_nodiratime_bd" off \
-	"relatime" "$_mnt_relatime_bd" off \
-	"nodev" "$_mnt_nodev_bd" off \
-	"nosuid" "$_mnt_nosuid_bd" off \
-	"noexec" "$_mnt_noexec_bd" off \
-	"ro" "$_mnt_ro_bd" off \
+	dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_filesystem " --checklist "$_Mnt_Body" 0 0 16 \
+	"async" "$_mnt_async_bd" off \
 	"sync" "$_mnt_sync_bd" off \
+	"nodev" "$_mnt_nodev_bd" off \
+	"nodiratime" "$_mnt_nodiratime_bd" off \
+	"noexec" "$_mnt_noexec_bd" off \
+	"noatime" "$_mnt_noatime_bd" off \
+	"relatime" "$_mnt_relatime_bd" off \
+	"norelatime" "$_mnt_norelatime_bd" off \
+	"nosuid" "$_mnt_nosuid_bd" off \
+	"ro" "$_mnt_ro_bd" off \
+	"rw" "$_mnt_rw_bd" off \
+	"acl" "$_mnt_acl_bd" off \
+	"noacl" "$_mnt_noacl_bd" off \
+	"errors=continue" "$_mnt_continue_bd" off \
+	"errors=panic" "$_mnt_panic_bd" off \
 	"usrquota" "$_mnt_usrquota_bd" off \
 	"grpquota" "$_mnt_grpqutoa_bd" off \
 	"user_xattr" "$_mnt_user_xattr_bd" off 2>${MOUNT_OPTS}
@@ -70,16 +83,23 @@ ext2_mounted_options()
 
 ext3_mounted_options()
 {
-	dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_filesystem " --checklist "$_Mnt_Body" 0 0 10 \
+	dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_filesystem " --checklist "$_Mnt_Body" 0 0 16 \
+	"async" "$_mnt_async_bd" off \
+	"sync" "$_mnt_sync_bd" off \
+	"nodev" "$_mnt_nodev_bd" off \
+	"nodiratime" "$_mnt_nodiratime_bd" off \
+	"noexec" "$_mnt_noexec_bd" off \
 	"noatime" "$_mnt_noatime_bd" off \
 	"relatime" "$_mnt_relatime_bd" off \
-	"nodev" "$_mnt_nodev_bd" off \
+	"norelatime" "$_mnt_norelatime_bd" off \
 	"nosuid" "$_mnt_nosuid_bd" off \
-	"noexec" "$_mnt_noexec_bd" off \
 	"ro" "$_mnt_ro_bd" off \
-	"sync" "$_mnt_sync_bd" off \
-	"usrquota" "$_mnt_usrquota_bd" off \
-	"grpquota" "$_mnt_grpqutoa_bd" off \
+	"rw" "$_mnt_rw_bd" off \
+	"acl" "$_mnt_acl_bd" off \
+	"norecovery" "$_mnt_norecovery_bd" off \
+	"data=journal" "$_mnt_journal_bd" off \
+	"data=ordered" "$_mnt_ordered_bd" off \
+	"data=writeback" "$_mnt_writeback_bd" off \
 	"user_xattr" "$_mnt_user_xattr_bd" off 2>${MOUNT_OPTS}
 	
 	# Now clean up the file
@@ -96,24 +116,28 @@ ext3_mounted_options()
 ext4_mounted_options()
 {
 	dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_filesystem " --checklist "$_Mnt_Body" 0 0 16 \
+	"async" "$_mnt_async_bd" off \
+	"sync" "$_mnt_sync_bd" off \
+	"nodev" "$_mnt_nodev_bd" off \
+	"nodiratime" "$_mnt_nodiratime_bd" off \
+	"noexec" "$_mnt_noexec_bd" off \
+	"noatime" "$_mnt_noatime_bd" off \
+	"relatime" "$_mnt_relatime_bd" off \
+	"norelatime" "$_mnt_norelatime_bd" off \
+	"nosuid" "$_mnt_nosuid_bd" off \
+	"ro" "$_mnt_ro_bd" off \
+	"rw" "$_mnt_rw_bd" off \
 	"journal" "$_mnt_journal_bd" off \
+	"data=journal" "$_mnt_journal_bd" off \
+	"data=ordered" "$_mnt_ordered_bd" off \
 	"data=writeback" "$_mnt_writeback_bd" off \
 	"delalloc" "$_mnt_delalloc_bd" off \
+	"nodelalloc" "$_mnt_nodelalloc_bd" off \
 	"discard" "$_mnt_discard_bd" off \
 	"noacl" "$_mnt_noacl_bd" off \
-	"noatime" "$_mnt_noatime_bd" off \
 	"nobarrier" "$_mnt_nobarrier_bd" off \
-	"nodelalloc" "$_mnt_nodelalloc_bd" off \
-	"nodiratime" "$_mnt_nodiratime_bd" off \
-	"relatime" "$_mnt_relatime_bd" off \
-	"nodev" "$_mnt_nodev_bd" off \
-	"nosuid" "$_mnt_nosuid_bd" off \
-	"noexec" "$_mnt_noexec_bd" off \
-	"ro" "$_mnt_ro_bd" off \
-	"sync" "$_mnt_sync_bd" off \
 	"usrquota" "$_mnt_usrquota_bd" off \
-	"grpquota" "$_mnt_grpqutoa_bd" off \
-	"user_xattr" "$_mnt_user_xattr_bd" off 2>${MOUNT_OPTS}
+	"grpquota" "$_mnt_grpqutoa_bd" off 2>${MOUNT_OPTS}
 	
 	# Now clean up the file
 	sed -i 's/ /,/g' ${MOUNT_OPTS}
@@ -128,7 +152,18 @@ ext4_mounted_options()
 
 f2fs_mounted_options()
 {
-	dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_filesystem " --checklist "$_Mnt_Body" 0 0 15 \
+	dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_filesystem " --checklist "$_Mnt_Body" 0 0 16 \
+	"async" "$_mnt_async_bd" off \
+	"sync" "$_mnt_sync_bd" off \
+	"nodev" "$_mnt_nodev_bd" off \
+	"nodiratime" "$_mnt_nodiratime_bd" off \
+	"noexec" "$_mnt_noexec_bd" off \
+	"noatime" "$_mnt_noatime_bd" off \
+	"relatime" "$_mnt_relatime_bd" off \
+	"norelatime" "$_mnt_norelatime_bd" off \
+	"nosuid" "$_mnt_nosuid_bd" off \
+	"ro" "$_mnt_ro_bd" off \
+	"rw" "$_mnt_rw_bd" off \
 	"data_flush" "$_mnt_data_flush_bd" off \
 	"disable_roll_forward" "$_mnt_disable_roll_forward_bd" off \
 	"disable_ext_identify" "$_mnt_disable_ext_identify_bd" off \
@@ -158,18 +193,22 @@ f2fs_mounted_options()
 
 jfs_mounted_options()
 {
-	dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_filesystem " --checklist "$_Mnt_Body" 0 0 11 \
+	dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_filesystem " --checklist "$_Mnt_Body" 0 0 15 \
+	"async" "$_mnt_async_bd" off \
+	"sync" "$_mnt_sync_bd" off \
+	"nodev" "$_mnt_nodev_bd" off \
+	"nodiratime" "$_mnt_nodiratime_bd" off \
+	"noexec" "$_mnt_noexec_bd" off \
+	"noatime" "$_mnt_noatime_bd" off \
+	"relatime" "$_mnt_relatime_bd" off \
+	"norelatime" "$_mnt_norelatime_bd" off \
+	"nosuid" "$_mnt_nosuid_bd" off \
+	"ro" "$_mnt_ro_bd" off \
+	"rw" "$_mnt_rw_bd" off \
 	"discard" "$_mnt_discard_bd" off \
 	"errors=continue" "$_mnt_continue_bd" off \
 	"errors=panic" "$_mnt_panic_bd" off \
-	"nointegrity" "$_mnt_nointegrity_bd" off \
-	"noatime" "$_mnt_noatime_bd" off \
-	"relatime" "$_mnt_relatime_bd" off \
-	"nodev" "$_mnt_nodev_bd" off \
-	"nosuid" "$_mnt_nosuid_bd" off \
-	"noexec" "$_mnt_noexec_bd" off \
-	"ro" "$_mnt_ro_bd" off \
-	"sync" "$_mnt_sync_bd" off 2>${MOUNT_OPTS}
+	"nointegrity" "$_mnt_nointegrity_bd" off 2>${MOUNT_OPTS}
 	
 	# Now clean up the file
 	sed -i 's/ /,/g' ${MOUNT_OPTS}
@@ -184,7 +223,18 @@ jfs_mounted_options()
 
 nilfs2_mounted_options()
 {
-	dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_filesystem " --checklist "$_Mnt_Body" 0 0 7 \
+	dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_filesystem " --checklist "$_Mnt_Body" 0 0 16 \
+	"async" "$_mnt_async_bd" off \
+	"sync" "$_mnt_sync_bd" off \
+	"nodev" "$_mnt_nodev_bd" off \
+	"nodiratime" "$_mnt_nodiratime_bd" off \
+	"noexec" "$_mnt_noexec_bd" off \
+	"noatime" "$_mnt_noatime_bd" off \
+	"relatime" "$_mnt_relatime_bd" off \
+	"norelatime" "$_mnt_norelatime_bd" off \
+	"nosuid" "$_mnt_nosuid_bd" off \
+	"ro" "$_mnt_ro_bd" off \
+	"rw" "$_mnt_rw_bd" off \
 	"discard" "$_mnt_discard_bd" off \
 	"nobarrier" "$_mnt_nobarrier_bd" off \
 	"errors=continue" "$_mnt_continue_bd" off \
@@ -207,6 +257,17 @@ nilfs2_mounted_options()
 reiserfs_mounted_options()
 {
 	dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_filesystem " --checklist "$_Mnt_Body" 0 0 5 \
+	"async" "$_mnt_async_bd" off \
+	"sync" "$_mnt_sync_bd" off \
+	"nodev" "$_mnt_nodev_bd" off \
+	"nodiratime" "$_mnt_nodiratime_bd" off \
+	"noexec" "$_mnt_noexec_bd" off \
+	"noatime" "$_mnt_noatime_bd" off \
+	"relatime" "$_mnt_relatime_bd" off \
+	"norelatime" "$_mnt_norelatime_bd" off \
+	"nosuid" "$_mnt_nosuid_bd" off \
+	"ro" "$_mnt_ro_bd" off \
+	"rw" "$_mnt_rw_bd" off \
 	"acl" "$_mnt_acl_bd" off \
 	"nolog" "$_mnt_nolog_bd" off \
 	"notail" "$_mnt_notail_bd" off \
@@ -227,11 +288,17 @@ reiserfs_mounted_options()
 vfat_mounted_options()
 {
 	dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_filesystem " --checklist "$_Mnt_Body" 0 0 7 \
-	"noatime" "$_mnt_noatime_bd" off \
-	"nodiratime" "$_mnt_nodiratime_bd" off \
-	"relatime" "$_mnt_relatime_bd" off \
-	"ro" "$_mnt_ro_bd" off \
+	"async" "$_mnt_async_bd" off \
 	"sync" "$_mnt_sync_bd" off \
+	"nodev" "$_mnt_nodev_bd" off \
+	"nodiratime" "$_mnt_nodiratime_bd" off \
+	"noexec" "$_mnt_noexec_bd" off \
+	"noatime" "$_mnt_noatime_bd" off \
+	"relatime" "$_mnt_relatime_bd" off \
+	"norelatime" "$_mnt_norelatime_bd" off \
+	"nosuid" "$_mnt_nosuid_bd" off \
+	"ro" "$_mnt_ro_bd" off \
+	"rw" "$_mnt_rw_bd" off \
 	"quiet" "$_mnt_quiet_bd" off \
 	"discard" "$_mnt_discard_bd" off 2>${MOUNT_OPTS}
 	
@@ -249,6 +316,17 @@ vfat_mounted_options()
 xfs_mounted_options()
 {
 	dialog --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " $_filesystem " --checklist "$_Mnt_Body" 0 0 16 \
+	"async" "$_mnt_async_bd" off \
+	"sync" "$_mnt_sync_bd" off \
+	"nodev" "$_mnt_nodev_bd" off \
+	"nodiratime" "$_mnt_nodiratime_bd" off \
+	"noexec" "$_mnt_noexec_bd" off \
+	"noatime" "$_mnt_noatime_bd" off \
+	"relatime" "$_mnt_relatime_bd" off \
+	"norelatime" "$_mnt_norelatime_bd" off \
+	"nosuid" "$_mnt_nosuid_bd" off \
+	"ro" "$_mnt_ro_bd" off \
+	"rw" "$_mnt_rw_bd" off \
 	"discard" "$_mnt_discard_bd" off \
 	"filestreams" "$_mnt_filestreams_bd" off \
 	"ikeep" "$_mnt_ikeep_bd" off \
@@ -258,13 +336,6 @@ xfs_mounted_options()
 	"norecovery" "$_mnt_norecovery_bd" off \
 	"noquota" "$_mnt_noquota_bd" off \
 	"wsync" "$_mnt_wsync_bd" off \
-	"noatime" "$_mnt_noatime_bd" off \
-	"relatime" "$_mnt_relatime_bd" off \
-	"nodev" "$_mnt_nodev_bd" off \
-	"nosuid" "$_mnt_nosuid_bd" off \
-	"noexec" "$_mnt_noexec_bd" off \
-	"ro" "$_mnt_ro_bd" off \
-	"sync" "$_mnt_sync_bd" off \
 	"usrquota" "$_mnt_usrquota_bd" off \
 	"grpquota" "$_mnt_grpqutoa_bd" off 2>${MOUNT_OPTS}
 	
